186437249dd302bbe0ced30976a2b2efbd56db99
anaisalfonsi <alfonsi_anais@hotmail.fr>
2021-02-27 11:04:16 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-filesystem",
       options: {
