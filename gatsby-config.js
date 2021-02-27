03d6b6adfb8ddee74470a4095934bc67bbfa455e
anaisalfonsi <alfonsi_anais@hotmail.fr>
2021-02-27 10:03:19 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: "Mantia_Architecture",
+  },
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
